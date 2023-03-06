823be24eb842942c833705084044220353f640de
Ani <ani.mkrtchyan1982@gmail.com>
2023-03-06 21:19:01 +0400

    Conditions (if, else)

--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalTruePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalTruePrinter.java
@@ -2,5 +2,13 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalTruePrinter {
     public static void main(String[] args) {
+        int a = 4;
+        int b = 5;
+        if (a < b) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
+
     }
 }
